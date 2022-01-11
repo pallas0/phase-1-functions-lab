44074542e8319f0212c001b80d6b800b0c89b9fb
amelia.risner0@gmail.com <arisner@berkeley.edu>
2022-01-11 00:10:16 -0800

    commit tree 2c63b65c8cbaa1f0c42d54635430987edbab57c9

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(value) {
+    return Math.abs(value - 42);
+}
+
+function distanceFromHqInFeet(value) {
+    return distanceFromHqInBlocks(value)* 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination)*264;
+}
+
+function calculatesFarePrice(start, destination) {
+    const dist = distanceTravelledInFeet(start, destination)
+    if (dist <= 400) {
+        return 0;
+    }
+    else if (dist < 2000) {
+        return .02*(dist - 400);
+    }
+    else if (dist > 2000 && dist < 2500) {
+        return 25;
+    }
+    else if (dist > 2500) {
+        return "cannot travel that far"
+    }
+}
